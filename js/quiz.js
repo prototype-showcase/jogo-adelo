749c82b1dee85c4ad00cbdcf9e7d87b9b3a183b8
Fábio Gouveia Silva <gouveia.fabio98@gmail.com>
2025-03-02 12:04:53 +0000

    image remove

--- js/quiz.js
+++ js/quiz.js
@@ -656,6 +656,10 @@ async function updateQuestion() {
 
             questionText.image.maskIcon = quizImageLoad.get(imgX, imgY, newW, newH);
             questionText.image.maskIcon.mask(maskedImage);
+
+
+            quizImageLoad.remove();
+            quizImageLoad = null;
         });
         maskedImage = undefined;
     }
